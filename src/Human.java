208dbb7edcf40676afb42e17988b8f64a45e1c0d
renikQ <trog94@mail.ru>
2022-10-30 22:15:04 +0500

    full completed homework

--- src/Human.java
+++ src/Human.java
@@ -2,15 +2,19 @@ public class Human {
     int yearOfBirth;
     String name;
     String town;
+    String jobTitle;
 
-    public Human(int yearOfBirth, String name, String town) {
+    public Human(int yearOfBirth, String name, String town, String jobTitle) {
         this.yearOfBirth = yearOfBirth;
         this.name = name;
         this.town = town;
+        this.jobTitle = jobTitle;
     }
 
     public void welcome() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Будем знакомы!");
+        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". " +
+                "Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!");
+
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maxim = new Human(1988, "Максим", "Минск");
+        Human maxim = new Human(1988, "Максим", "Минск", "бренд-менеджер");
         maxim.welcome();
-        Human anna = new Human(1993, "Аня", "Москва");
+        Human anna = new Human(1993, "Аня", "Москва", "методист образовательных программ");
         anna.welcome();
-        Human katya = new Human(1992, "Катя", "Калининград");
+        Human katya = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
         katya.welcome();
-        Human artem = new Human(1995, "Артём", "Москва");
+        Human artem = new Human(1995, "Артём", "Москва", "директор по развитию бизнеса");
         artem.welcome();
 
     }
