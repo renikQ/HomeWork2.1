b2b05da7d2638f582f401b7aa6f4aa270529a2ab
renikQ <trog94@mail.ru>
2022-10-30 21:56:38 +0500

    completed homework

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Human maxim = new Human(1988, "Максим", "Минск");
-        Human anna = new Human(1993, "Аня", "Москва");
-        Human katya = new Human(1992, "Катя", "Калининград");
-        Human artem = new Human(1995, "Артём", "Москва");
         maxim.welcome();
+        Human anna = new Human(1993, "Аня", "Москва");
         anna.welcome();
+        Human katya = new Human(1992, "Катя", "Калининград");
         katya.welcome();
+        Human artem = new Human(1995, "Артём", "Москва");
         artem.welcome();
 
     }
